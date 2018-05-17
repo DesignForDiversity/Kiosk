2a86e02e5c60a6f43dbbc0dffd2f7dc256476889
violet elder <vmelder@ucdavis.edu>
2018-05-17 14:09:46 -0700

    Animations fixed, alphabetical order!

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 //should it be like... a switch that changes the background color and subs in the correct text?
 
 
-var sandralink = document.getElementById('link1');
+var sandralink = document.getElementById('link2');
 var sandraoverlay = document.getElementById('sandra');
 
-var annielink = document.getElementById('link2');
+var annielink = document.getElementById('link1');
 var annieoverlay = document.getElementById('annie');
 
-var emilylink = document.getElementById('link3');
+var emilylink = document.getElementById('link4');
 var emilyoverlay = document.getElementById('emily');
 
-var fionalink = document.getElementById('link4');
+var fionalink = document.getElementById('link3');
 var fionaoverlay = document.getElementById('fiona');
 
-var crystallink = document.getElementById('link5');
+var crystallink = document.getElementById('link10');
 var crystaloverlay = document.getElementById('crystal');
 
-var cherilynlink = document.getElementById('link6');
+var cherilynlink = document.getElementById('link9');
 var cherilynoverlay = document.getElementById('cherilyn');
 
 var ellelink = document.getElementById('link7');
 var elleoverlay = document.getElementById('elle');
 
-var anthonylink = document.getElementById('link8');
+var anthonylink = document.getElementById('link5');
 var anthonyoverlay = document.getElementById('anthony');
 
-var zhaohuilink = document.getElementById('link9');
+var zhaohuilink = document.getElementById('link8');
 var zhaohuioverlay = document.getElementById('zhaohui');
 
-var darialink = document.getElementById('link10');
+var darialink = document.getElementById('link6');
 var dariaoverlay = document.getElementById('daria');
 
-sandralink.addEventListener('click', function(){
-  sandraoverlay.style.display="block";
-  annieoverlay.style.display="none";
+annielink.addEventListener('click', function(){
+  sandraoverlay.style.display="none";
+  annieoverlay.style.display="block";
   emilyoverlay.style.display="none";
   fionaoverlay.style.display="none";
   crystaloverlay.style.display="none";
@@ -44,9 +44,9 @@ sandralink.addEventListener('click', function(){
   dariaoverlay.style.display="none";
 })
 
-annielink.addEventListener('click', function(){
-  sandraoverlay.style.display="none";
-  annieoverlay.style.display="block";
+sandralink.addEventListener('click', function(){
+  sandraoverlay.style.display="block";
+  annieoverlay.style.display="none";
   emilyoverlay.style.display="none";
   fionaoverlay.style.display="none";
   crystaloverlay.style.display="none";
