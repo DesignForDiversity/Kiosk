706aa0053dc512a9e770f2c80888ea4972e865f1
Tara Parker-Essig <tparkeressig@ucdavis.edu>
2018-05-17 23:13:51 -0700

    Updated sandra's check yoself 7:30 code

--- sites/check-yoself/script.js
+++ sites/check-yoself/script.js
@@ -4,6 +4,51 @@ var textarea = document.getElementById("myTextarea");
 var button = document.getElementById('button');
 var picture = document.getElementById('picture');
 var welcome = document.getElementById('welcome');
+var speech = document.getElementsByClassName('speech-bubble')[0];
+var speechText = document.getElementById('speech-text');
+
+var idleTime = 0;
+$(document).ready(function () {
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 1000); // 5 seconds
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+        speech.style.display = 'none';
+    });
+});
+
+var useTimerIncrement = true;
+function timerIncrement() {
+  if (!useTimerIncrement) {
+    return;
+  }
+    idleTime = idleTime + 1;
+    // console.log(idleTime);
+    var timeIn = 2;
+    var inToOut = 8;
+    var outToIn = 8;
+
+    if ((idleTime - timeIn) % (inToOut + outToIn) == 0) { // 20 minutes
+      // console.log("wow.")
+      speech.style.display = 'block';
+      fadeIn(speech, 2000);
+
+
+      var text = ['\"Boo, you whore!\"', '\"She\'s an illegal.\"', '\"That\'s so gay.\"', '\"No homo.\"', '\"You\'re retarded.\"', '\"That\'s ghetto.\"' , '\"What a whore.\"', '\"What a slut.\"', '\"What a hoe.\"' ];
+      var rand = text[Math.floor(Math.random() * text.length)];
+
+      var str = "If you're not sure what to type, try " + rand;
+      speechText.innerHTML = str;
+    }
+    if ((idleTime - timeIn - inToOut) % (inToOut + outToIn) == 0) { // 20 minutes
+      // console.log("wow.")
+      fadeOut(speech, 2000);
+
+    }
+}
 
 function fadeOut(el1, time) {
   el1.style.opacity = 1;
@@ -21,6 +66,12 @@ function fadeOut(el1, time) {
   tick();
 }
 
+function disappear(el1) {
+  // console.log("you're in me");
+
+  el1.style.opacity = 0.1;
+  el1.style.display = 'none';
+}
 
 function fadeIn(el1, time) {
   el1.style.opacity = 0;
@@ -40,6 +91,8 @@ function fadeIn(el1, time) {
 
 
 function myFunction() {
+  useTimerIncrement = false;
+  disappear(speech);
   var textarea = document.getElementById("myTextarea");
   var button = document.getElementById('button');
   var picture = document.getElementById('picture');
@@ -56,6 +109,7 @@ function myFunction() {
   fadeOut(button, timeout);
   fadeOut(title, timeout);
   fadeOut(welcome, timeout);
+
 
   setTimeout(function() {
     textarea.parentNode.removeChild(textarea);
@@ -82,11 +136,11 @@ function myFunction() {
       elm3 = elms[i+2];
     }
 
-    if (elm.toLowerCase() == 'no' && elm2.toLowerCase() == 'homo' ) {
+    if (elm.toLowerCase() == 'no' && elm2.toLowerCase().indexOf('homo') >= 0) {
       elm = elm + " " + elm2;
       i++; // skip next loop
     }
-    if (elm.toLowerCase() == 'that\'s' && elm2.toLowerCase() == 'so' && elm3.toLowerCase() == 'gay' ) {
+    if (elm.toLowerCase() == 'that\'s' && elm2.toLowerCase() == 'so' && elm3.toLowerCase().indexOf('gay') >= 0) {
       elm = elm + " " + elm2 + " " + elm3;
       i+=2; // skip next loop
     }
@@ -112,10 +166,10 @@ function myFunction() {
       elmWithTag = "<span class='non-highlighted'>" + elm + specialWord + "</span>";
     }
     txtWithTag += elmWithTag + " ";
-    console.log(txtWithTag);
+    // console.log(txtWithTag);
     // console.log(txtWithTag);
     document.getElementById("demo").innerHTML = txtWithTag;
-    console.log(document.getElementById("demo"));
+    // console.log(document.getElementById("demo"));
   }
 }
 
@@ -130,7 +184,7 @@ function createElementFromHTML(htmlString) {
 
 function highlight(word){
   var result = word.match(/illegal|bitch|ghetto|retarted|lame|whore|slut|hoe|no|homo|that|so|gay/i);
-  console.log(result);
+  // console.log(result);
   return result;
   // console.log(result)
 }
@@ -139,6 +193,8 @@ function highlight(word){
 
 function toggleHighlight(thisDom) {
     var word = thisDom;
+
+    // console.log(word);
     var link = document.getElementsByClassName("foo")[0];
     var explanation =document.getElementById("t-explanation");
 
@@ -150,34 +206,34 @@ function toggleHighlight(thisDom) {
         // word.className += " highlight";
         // link.classList.remove("link-1");
         link.style.textDecoration = "none";
-        if(word.innerHTML == 'bitch'){
+        if(word.innerHTML.toLowerCase() == 'bitch'){
           explanation.innerHTML = "The word <strong>\"bitch\"</strong> is a word which is used culturally to describe any woman who is outspoken, or strong, or uncompromising, in a derogatory manner.";
         }
-        else if(word.innerHTML == 'illegal'){
+        else if(word.innerHTML.toLowerCase() == 'illegal'){
           explanation.innerHTML = "The word <strong>\"illegal\"</strong> has been applied and abused by those advocating for harsh immigration policies that are racist and xenophobic. No one is illegal for living.";
         }
-        else if(word.innerHTML == 'ghetto'){
-          explanation.innerHTML = "The word <strong>\"ghetto\"</strong> describes something or someone as cheap, worn out, poor, dangerous, etc. Reference to housing communities that are impoverished and disproportionately impact of people of color. Associates people of color with these negative characteristics";
+        else if(word.innerHTML.toLowerCase() == 'ghetto'){
+          explanation.innerHTML = "The word <strong>\"ghetto\"</strong> describes something or someone as cheap, worn out, poor, dangerous, etc. Reference to housing communities that are impoverished and disproportionately impact of people of color. Associates people of color with these negative characteristics.";
         }
-        else if(word.innerHTML == 'homo'){
+        else if(word.innerHTML.toLowerCase() == 'homo'){
           explanation.innerHTML = "The word <strong>\"homo\"</strong> stigmatizes gay and queer people. Uses their identities to describe something as undesirable and bad. Replaces negative adjectives with words related to queer/LGBT identity.";
         }
-        else if(word.innerHTML == 'retarted'){
-          explanation.innerHTML = "The word <strong>\"retarded\"</strong> targets mental, emotional, and physical disabilities as objects for ridicule. Used as synonyms for “worthless”, “bad”, “unintelligent”, “incapable”, etc";
+        else if(word.innerHTML.toLowerCase() == 'retarted'){
+          explanation.innerHTML = "The word <strong>\"retarded\"</strong> targets mental, emotional, and physical disabilities as objects for ridicule. Used as synonyms for “worthless”, “bad”, “unintelligent”, “incapable”, etc.";
         }
-        else if(word.innerHTML == 'whore'){
-          explanation.innerHTML = "The word <strong>\"whore\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it";
+        else if(word.innerHTML.toLowerCase() == 'whore'){
+          explanation.innerHTML = "The word <strong>\"whore\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it.";
         }
-        else if(word.innerHTML == 'hoe'){
-          explanation.innerHTML = "The word <strong>\"hoe\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it";
+        else if(word.innerHTML.toLowerCase() == 'hoe'){
+          explanation.innerHTML = "The word <strong>\"hoe\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it.";
         }
-        else if(word.innerHTML == 'slut'){
-          explanation.innerHTML = "The word <strong>\"slut\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it";
+        else if(word.innerHTML.toLowerCase() == 'slut'){
+          explanation.innerHTML = "The word <strong>\"slut\"</strong> dismisses anyone seen as being “too” sexual —particular sex workers, women, queer people, and people of color. Perpetuates negativity towards sex itself. Regulates who is allowed to have it.";
         }
-        else if(word.innerHTML == 'that\'s so gay'){
+        else if(word.innerHTML.toLowerCase() == 'that\'s so gay'){
           explanation.innerHTML = "The phrase <strong>\"that\'s so gay\"</strong> stigmatizes gay and queer people. Uses their identities to describe something as undesirable and bad. Replaces negative adjectives with words related to queer/LGBT identity.";
         }
-        else if(word.innerHTML == 'no homo'){
+        else if(word.innerHTML.toLowerCase() == 'no homo'){
           explanation.innerHTML = "The phrase <strong>\"no homo\"</strong> stresses the speaker’s heterosexuality, masculinity, and/or other traits to avoid being perceived as gay/queer. Goes to great lengths to avoid association with anything queer, and ultimately reinforces the notion that queer is a negative characteristic.";
         }
     }
